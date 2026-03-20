40f3dcc0dc381904ce9d37902be838624e0324a2
unknown <08250268.sce@rub.edu.bt>
2026-03-20 16:16:39 +0600

    student exercise

---
+++ demo/stdEx.py
@@ -0,0 +1,14 @@
+print("Welcome")
+name = (input("Please Enter your name:"));
+print(name)
+num1 = float(input("Enter the first number:"));
+num2 = float(input("Enter the second number:"));
+addition_result = num1 + num2
+subtraction_result = num1 - num2
+multiplication_result = num1 * num2
+division_result = num1 / num2
+print (addition_result)
+print(subtraction_result)
+print(multiplication_result)
+print(division_result)
+print("Thank you")
