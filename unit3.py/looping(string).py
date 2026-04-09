59d306ebdc0f2551dfe5302ca6ec29d93c4a7f9f
unknown <08250268.sce@rub.edu.bt>
2026-04-09 16:17:44 +0600

    Looping through string

---
+++ unit3.py/looping(string).py
@@ -0,0 +1,12 @@
+name=input("Enter your name: ")
+for i in name:
+    print (i)
+
+li=("Python Programming", "Python Fundamentals","Python Interview Questions")
+for x in li: #x is a variable that takes the value of each item in the list li during each iteration of the loop
+    print(x)
+
+lenli=len(li)
+for x in range(lenli):
+    print(li[x])
+                                                                                                                                                                                          
