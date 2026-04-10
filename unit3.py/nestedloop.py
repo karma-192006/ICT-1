a37381fa7f824a8a4dd249223d1916a6db1c0a8d
unknown <08250268.sce@rub.edu.bt>
2026-04-10 15:58:47 +0600

    nested loop

--- unit3.py/nestedloop.py
+++ unit3.py/nestedloop.py
@@ -1,4 +1,9 @@
 for i in range(1,4):
     for j in range(i):
         print(f"Outer Loop iteration{i}, inner loop iteration{j+1}")
-        
+
+for i in range(4):
+    for j in range(i):
+        print("*",end="")
+    print()
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
