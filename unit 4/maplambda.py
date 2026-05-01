c71c691a0fa8b4ad6abfad451d178a3889e08aeb
unknown <08250268.sce@rub.edu.bt>
2026-05-01 16:04:00 +0600

    map lambda

--- unit 4/maplambda.py
+++ unit 4/maplambda.py
@@ -1,11 +1,13 @@
 mylist=[1,2,3,4]
 double=map(lambda x: x*2, mylist)
-print(list(double))
+#print(list(double))
 
 #converting the result of double back to list 
 mynewlist=(list(double))
-back=map(lambda x: x/2, mynewlist)
-print(mylist)
+print(mynewlist)
+division=map(lambda x: x/2, mynewlist)
+print(list(division))
+
 
 
 
